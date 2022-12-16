23d1d625da1453fc0d73fcb5ab9beee923cc07ce
Rosario Martinez <rosariomartinez927@yahoo.com>
2022-12-15 22:19:20 -0300

    Actualizacion

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 AOS.init()
+
+const main = document.getElementById("main")
+const nav = document.getElementById("nav-menu")
+const navItems = document.getElementsByClassName("nav-item")
+
+window.addEventListener("scroll", function() {
+
+    let posicionMain = main.getBoundingClientRect().top
+
+    if(posicionMain <= -580) {
+        nav.classList.add("bg-nav")
+    } else {
+        nav.classList.remove("bg-nav")
+    }
+})
